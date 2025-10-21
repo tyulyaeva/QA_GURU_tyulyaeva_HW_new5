0f45ae4e3eb2218825d4ad5184d08d79cdb7ae1f
Tyulyaeva Inna <tyulyaeva.inna@yandex.ru>
2025-10-21 14:42:57 +0300

    first

---
+++ google_should_find_selene.py
@@ -0,0 +1,5 @@
+from selene import browser, be, have
+
+browser.open('https://google.com')
+browser.element('[name="q"]').should(be.blank).type('qa.guru').press_enter()
+browser.element('html').should(have.text('About this page'))
